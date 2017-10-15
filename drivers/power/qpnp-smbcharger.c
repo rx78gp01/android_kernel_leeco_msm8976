5a8d6431e0aeb81520e226aa8a5f4d6166bd245f
google <android@google.com>
2017-10-14 20:38:39 -0700

    drivers: power: qpnp-smbcharger:reduce default_hvdcp_icl_ma

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -396,7 +396,7 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_hvdcp_icl_ma = 3000;
+static int smbchg_default_hvdcp_icl_ma = 2000;
 module_param_named(
 	default_hvdcp_icl_ma, smbchg_default_hvdcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
